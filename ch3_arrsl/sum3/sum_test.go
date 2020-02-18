74562afe300cb443b67d8304eeb91bf8941f1d40
Marijus Ravickas <MRAVI@danskebank.lt>
2020-02-18 14:03:49 +0200

    adding SumAll

--- ch3_arrsl/sum3/sum_test.go
+++ ch3_arrsl/sum3/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -12,4 +15,15 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	firstSlice := []int{1, 2}
+	secondSlice := []int{0, 9}
+	got := SumAll(firstSlice, secondSlice)
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) { // reflect.DeepEqual is not type safe, allowas to compare string to int as example
+		t.Errorf("got %v want %v", got, want)
+	}
 }
