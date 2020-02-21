c7a51239147028907b5e3c346e65543dcbd18522
Marijus Ravickas <MRAVI@danskebank.lt>
2020-02-21 12:30:36 +0200

    finished ch4

---
+++ ch4_str_meth_int/shapes/shapes.go
@@ -0,0 +1,36 @@
+package main
+
+import "math"
+
+//Rectangle contains info about rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+//Area returns are asize of rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+//Circle contains info about circle
+type Circle struct {
+	Radius float64
+}
+
+//Area returns are asize of circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Perimeter returns perimeter value of rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	var perimeter = 2 * (rectangle.Width + rectangle.Height)
+	return perimeter
+}
+
+//Area returns are size of rectangle
+func Area(rectangle Rectangle) float64 {
+	var area = rectangle.Width * rectangle.Height
+	return area
+}
